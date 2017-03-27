e1c930d3db660687168aa6ee5b636212c720cdda
Quan Vo <quan.vo@ibm.com>
2017-03-27 16:06:22 -0500

    Cast emailRegex as CVarArg if on Linux.

--- Sources/SMTPSender.swift
+++ Sources/SMTPSender.swift
@@ -133,7 +133,7 @@ private extension SMTPSender {
 
 private extension String {
     #if os(Linux)
-    static let emailRegex: NSString = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+    static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}" as CVarArg
     #else
     static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
     #endif
