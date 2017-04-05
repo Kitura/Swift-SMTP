037eaee294092291255864a9ff309852e0a0d11b
Quan Vo <quan.vo@ibm.com>
2017-04-05 14:44:12 -0500

    Randomize tests on Linux

--- Tests/KituraSMTPTests/TestAuthEncoder.swift
+++ Tests/KituraSMTPTests/TestAuthEncoder.swift
@@ -14,6 +14,8 @@
  * limitations under the License.
  **/
 
+import Foundation
+
 import XCTest
 @testable import KituraSMTP
 
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,8 +2,8 @@ import XCTest
 @testable import KituraSMTPTests
 
 XCTMain([
-     testCase(TestAuthEncoder.allTests),
-     testCase(TestDataSender.allTests),
-     testCase(TestLogin.allTests),
-     testCase(TestSender.allTests)
+     testCase(TestAuthEncoder.allTests.shuffled()),
+     testCase(TestDataSender.allTests.shuffled()),
+     testCase(TestLogin.allTests.shuffled()),
+     testCase(TestSender.allTests.shuffled())
 ])
