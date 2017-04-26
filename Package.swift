be65a7a80ea50619397cd48102192edd22e6a5bc
Quan Vo <quan.vo@ibm.com>
2017-04-26 12:48:11 -0500

    Add BlueSocket as dependency

--- Package.swift
+++ Package.swift
@@ -6,9 +6,10 @@ print("Swift 3.1 not supported on Linux. Try Swift 3.0.2 or Swift >=3.1.1.")
 let package = Package(
     name: "KituraSMTP",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1, minor: 7)
     ]
 )
 #endif
