348a0f1a40402f8f65b5e94c50e07f4312222b63
Quan Vo <quan.vo@ibm.com>
2017-10-04 11:57:59 -0500

    add swift 4 support

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -14,10 +14,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "0.12.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "0.12.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.0"),
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.1"),
+        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMinor(from: "0.12.0")),
+        .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", .upToNextMinor(from: "0.12.0")),
+        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .upToNextMinor(from: "0.8.0")),
+        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMinor(from: "1.7.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
